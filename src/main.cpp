0715212a72fe791f7f1838a5d43e19cd2df1a22a
Panos Kousidis <kouspana@csd.auth.gr>
2020-06-04 16:00:23 +0300

    pretty printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <sstream>
 #include "BinaryTree.h"
 #include "AVLTree.h"
 #include "Reader.h"
@@ -11,8 +12,8 @@ int main() {
     ofstream out("output.txt", ios::trunc); //delete previous contents of file
     out.close();
     out.open("output.txt", ios::app);
-    out << "RESULTS BELOW OF EACH SET OF WORDS\n" << endl;
-
+    out << "TIME RESULTS AT THE END OF FILE\n" << endl;
+    stringstream temp{};
     BinaryTree Btree;
     AVLTree tree;
     HashTable ht(20639);
@@ -22,38 +23,45 @@ int main() {
     snip.cleanFile();
     int s_w = snip.wordCount();
     //Binary Tree output
+    out << "------RESULTS OF BINARY TREE SEARCH------" << endl;
     auto now = chrono::steady_clock::now();
     book.build(Btree);
     auto mid = chrono::steady_clock::now();
     int found = snip.bulkSearch(Btree);
     auto end = chrono::steady_clock::now();
-    out << "\nBinary Tree: { \n\t" << "total words: " << Btree.count(Btree.getRoot()) << endl;
-    out << "\tunique words: " << Btree.uniqueCount(Btree.getRoot()) << endl;
-    out << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
-    out << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
-    out << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
+    out << "------END OF BINARY TREE SEARCH------\n" << endl;
+    temp << "\nBinary Tree: { \n\t" << "total words: " << Btree.count(Btree.getRoot()) << endl;
+    temp << "\tunique words: " << Btree.uniqueCount(Btree.getRoot()) << endl;
+    temp << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
+    temp << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
+    temp << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
     //AVL Tree output
+    out << "------RESULTS OF AVL TREE SEARCH------" << endl;
     now = chrono::steady_clock::now();
     book.build(tree);
     mid = chrono::steady_clock::now();
     found = snip.bulkSearch(tree);
     end = chrono::steady_clock::now();
-    out << "\nAVL Tree: { \n\t" << "total words: " << Btree.count(tree.getRoot()) << endl;
-    out << "\tunique words: " << Btree.uniqueCount(tree.getRoot()) << endl;
-    out << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
-    out << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
-    out << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
+    out << "------END OF AVL TREE SEARCH------\n" << endl;
+    temp << "\nAVL Tree: { \n\t" << "total words: " << Btree.count(tree.getRoot()) << endl;
+    temp << "\tunique words: " << Btree.uniqueCount(tree.getRoot()) << endl;
+    temp << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
+    temp << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
+    temp << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
     //HashTable output
+    out << "------RESULTS OF HASHTABLE SEARCH------" << endl;
     now = chrono::steady_clock::now();
     book.build(ht);
     mid = chrono::steady_clock::now();
     found = snip.bulkContains(ht);
     end = chrono::steady_clock::now();
-    out << "\nHashTable: { \n\t" << "filled %: " << ht.loadFactor() << "%" << endl;
-    out << "\tunique words: " << ht.getSize() << endl;
-    out << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
-    out << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
-    out << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
-
+    out << "------END OF HASHTABLE SEARCH------\n" << endl;
+    temp << "\nHashTable: { \n\t" << "filled %: " << ht.loadFactor() << "%" << endl;
+    temp << "\tunique words: " << ht.getSize() << endl;
+    temp << "\tbuild time: " << chrono::duration_cast<chrono::milliseconds>(mid - now).count() << " ms" << endl;
+    temp << "\tsearched words: " << s_w << ".\tfound: " << found << endl;
+    temp << "\tsearch time: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms\n}" << endl;
+    out << "------TIME RESULTS------\n" << endl;
+    out << temp.str();
     return 0;
 }
